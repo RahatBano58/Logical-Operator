15ea1d082bf3da0457f915a4888973ada73948ea
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-19 21:18:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// logical operators //
+// &&  operator//
+var num1 = 9;
+var num2 = 8;
+console.log((num1 > num2) &&
+    (num2 < num1));
+var num5 = 10;
+var num6 = 15;
+console.log((num5 < num6) &&
+    (num6 > num5));
+var num01 = 15;
+var num02 = 25;
+console.log((num01 < num02) &&
+    (num02 > num01));
+// Or Operator || 
+var num3 = 9;
+var num4 = 8;
+console.log((num3 < num4) ||
+    (9 == 9));
+var num7 = 10;
+var num8 = 12;
+console.log((num8 > num7) ||
+    (num7 == num7));
+var num9 = 25;
+var num10 = 22;
+console.log((num9 > num10) ||
+    (25 < 22));
+var num00 = 6;
+var num05 = 12;
+console.log((num05 > num00) ||
+    (12 == 12));
+// Nor Operator ! //
+var num03 = 19;
+var num04 = 29;
+console.log(!(num04 > num03));
+var num06 = 26;
+var num07 = 81;
+console.log(!(num06 < num07));
+var num08 = "Orange";
+var num09 = "Apple";
+console.log(!(num08 == num09));
+var num = "school";
+var num000 = "college";
+console.log(!(num == num000));
---
+++ app.ts
@@ -0,0 +1,82 @@
+ // logical operators //
+
+// &&  operator//
+
+let num1:number = 9;
+let num2:number = 8;
+console.log(
+    (num1 > num2) &&
+    (num2 < num1)
+);
+
+
+let num5:number = 10;
+let num6:number = 15;
+console.log(
+    (num5 < num6) &&
+    (num6 > num5)
+);
+
+
+let num01:number = 15;
+let num02:number = 25
+console.log(
+    (num01 < num02) &&
+    (num02 > num01)
+);
+
+
+// Or Operator || 
+let num3:number = 9;
+let num4:number = 8;
+console.log(
+    (num3 < num4) ||
+    (9 == 9)
+);
+
+let num7:number = 10;
+let num8:number = 12;
+console.log(
+    (num8 > num7) ||
+    (num7 == num7)
+);
+
+let num9:number = 25
+let num10:number = 22;
+console.log(
+    (num9 > num10) ||
+    (25 < 22)
+)
+let num00:number = 6
+let num05:number =12
+console.log(
+    (num05 > num00) ||
+    (12 == 12)
+)
+
+// Nor Operator ! //
+let num03:number = 19;
+let num04:number = 29;
+console.log(
+    !(num04 > num03)
+);
+
+let num06:number = 26;
+let num07:number = 81;
+
+console.log(
+    !(num06 < num07)
+);
+
+let num08:String ="Orange";
+let num09:string = "Apple";
+console.log(
+    !(num08 == num09)
+);
+
+let num:String = "school";
+let num000:string = "college";
+console.log(
+    !(num ==
+         num000)
+);
